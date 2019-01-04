5fad444430d53c584c6a5f8f6ab5be0adce9f2d7
alex-avila <alex.avilx@icloud.com>
2019-01-04 03:10:19 -0800

    first commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <header>
+    <nav class="nav">
+      <div class="nav__wrapper">
+        <div class="nav__logo">Avila Tile</div>
+        <div class="nav__links">
+          <nuxt-link
+            class="nav__link"
+            to="/"
+          >About</nuxt-link>
+          <nuxt-link
+            class="nav__link"
+            to="/"
+          >Portfolio</nuxt-link>
+          <nuxt-link
+            class="nav__link"
+            to="/"
+          >Blog</nuxt-link>
+          <nuxt-link
+            class="nav__link"
+            to="/"
+          >Contact</nuxt-link>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+
+<style lang="sass" scoped>
+.nav
+  padding: 1em
+  color: white
+
+.nav__wrapper
+  max-width: 1000px
+  margin: auto
+  display: flex
+  justify-content: space-between
+
+.nav__logo
+  letter-spacing: 3px
+  text-transform: uppercase
+  font-weight: 600
+
+.nav__link
+  display: inline
+  text-decoration: none
+  color: white
+  border-bottom: 1px solid white;
+
+  &:not(:last-child)
+    margin-right: 1em
+  
+  &:visited
+    color: white
+
+  &:hover
+    border-bottom: none;
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="container">
+    <div class="hero">
+      <div class="hero__overlay" />
+      <img
+        src="@/assets/background-large.jpg"
+        alt=""
+      >
+    </div>
+    <div>
+      <h1 class="title">Avila Tile</h1>
+    </div>
+  </section>
+</template>
+
+<style>
+.container {
+  min-height: calc(100vh - 50px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: white;
+}
+
+.hero {
+  position: absolute;
+  height: 100vh;
+  top: 0;
+  overflow: hidden;
+  z-index: -1;
+}
+
+.hero img {
+  max-width: none;
+  height: 100%;
+  position: relative;
+  z-index: -1;
+}
+
+.hero__overlay {
+  height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.25);
+  position: absolute;
+}
+
+.title {
+  font-family: 'Futura', 'Avenir', 'Source Sans Pro', -apple-system,
+    BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
