b3ce7a3d622fd9c1516107d15f15ba02696d9bc2
alex-avila <alex.avilx@icloud.com>
2019-03-02 00:18:30 -0800

    Add about page and a heading component

---
+++ components/MainHeading.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main-header__wrapper">
+    <h1 class="main-header">
+      <slot />
+      <!-- <span class="main-header__underline" /> -->
+    </h1>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+@import '../assets/styles/components/main-header'
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <article class="about">
+    <main-heading>About</main-heading>
+    <div class="default-wrapper">
+      <p class="paragraph">
+        Established in 2012, Avila Tiling prides ourselves in delivering our best work to
+        your business or residential improvement. Our clientele has built itself over the last
+        7 years based off of recommendation, professionalism and attention to detail. Make
+        sure to contact us to get your name on our books next!
+      </p>
+      <p class="paragraph">
+        Avila Tiling started when our CEO, Cristian Avila, brought his dream to life. He has
+        been in the industry for 10 years, in which he has gained much experience and
+        learned many tricks of the trade. After working for other people for 3 years, Cristian
+        was ready to take on his own projects.
+      </p>
+      <p class="paragraph">
+        The company is currently expanding and is excited to bring on more team members
+        in order to better service your needs. While we are a small company, big projects
+        keep us busy.
+      </p>
+      <p class="paragraph">
+        Our ability to expanding has been by recommendation and word of mouth from our
+        clientele, of which we take great pride in not only building your dream project, but
+        also in building repertoire with you. We believe the better we understand your
+        needs, the happier you’ll be with the finished product.
+        From kitchens, floors and bathrooms all the way to commercial properties, Avila
+      </p>
+      <p class="paragraph">
+        Tiling makes sure every detail is perfect so that your home and/or business goes
+        from a dream to reality.
+      </p>
+    </div>
+  </article>
+</template>
+
+<script>
+import MainHeading from '@/components/MainHeading.vue'
+
+export default {
+  components: { MainHeading }
+}
+</script>
+
+
+<style lang="sass" scoped>
+@import '../../assets/styles/pages/about'
+</style>
