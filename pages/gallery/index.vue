438a08b344fde20904bddcfd76c2cbe18d35b58c
alex-avila <alex.avilx@icloud.com>
2019-03-17 13:55:10 -0700

    Create the gallery page with an image grid similar to instagram

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <main-heading>Gallery</main-heading>
+  <section>
+    <main-heading>Gallery</main-heading>
+    <div class="gallery__container">
+      <div
+        v-for="({ src }, i) in galleryImages"
+        :key="src + i"
+        class="gallery__image-container"
+      >
+        <img
+          :src="src"
+          class="gallery__image"
+          alt=""
+        >
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -8,6 +23,38 @@ import MainHeading from '@/components/MainHeading.vue'
 export default {
   components: {
     MainHeading
+  },
+
+  data() {
+    return {
+      galleryImages: [
+        { src: this.getImage('70s') },
+        { src: this.getImage('blue-bathroom') },
+        { src: this.getImage('checkers') },
+        { src: this.getImage('checkers2') },
+        { src: this.getImage('white-backsplash') },
+        { src: this.getImage('white-tile') },
+        { src: this.getImage('wood-tile') },
+        { src: this.getImage('70s') },
+        { src: this.getImage('blue-bathroom') },
+        { src: this.getImage('checkers') },
+        { src: this.getImage('checkers2') },
+        { src: this.getImage('white-backsplash') },
+        { src: this.getImage('white-tile') },
+        { src: this.getImage('wood-tile') },
+        { src: this.getImage('70s') }
+      ]
+    }
+  },
+
+  methods: {
+    getImage(image) {
+      return require(`@/assets/images/featured-work/${image}.jpg`)
+    }
   }
 }
 </script>
+
+<style lang="sass" scoped>
+@import '../../assets/styles/pages/gallery'
+</style>
